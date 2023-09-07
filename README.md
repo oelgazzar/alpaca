2527a8ae0c97e07f0f25ac7e261693f8381eb620
omar elgazzar <omelgazzar@gmail.com>
2023-09-07 22:22:46 +0300

    add screenshot functionality

---
+++ README.md
@@ -0,0 +1,5 @@
+# Alpaca Image generator
+
+Built with React
+
+![Alt App Screenshot](screenshot.png)
---
+++ screenshot.js
@@ -0,0 +1,18 @@
+const puppeteer = require("puppeteer");
+
+let browser;
+(async () => {
+  browser = await puppeteer.launch({ headless: "new" });
+  let page = await browser.newPage();
+  await page.goto("http://localhost:3000/");
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 5000);
+  });
+  await page.screenshot({
+    path: `screenshot.png`,
+  });
+})().finally(() => {
+  if (browser) browser.close();
+});
